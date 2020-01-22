79adac273f60e979e251f7010985623aac121b08
Jonathan Washington <Jon.Washington@outlook.com>
2020-01-22 12:19:20 -0600

    router

--- ag-site/src/App.js
+++ ag-site/src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
-import Header from "./common/Header";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import IndexPage from "./pages";
+import {
+  HOME_PAGE_ROUTE,
+  EVENTS_PAGE_ROUTE,
+  ABOUT_PAGE_ROUTE,
+  CONTACT_PAGE_ROUTE
+} from "./common/consts/routes";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route path={CONTACT_PAGE_ROUTE}>Contact</Route>
+          <Route path={ABOUT_PAGE_ROUTE}>About</Route>
+          <Route path={EVENTS_PAGE_ROUTE}>Events</Route>
+          <Route path={HOME_PAGE_ROUTE}>
+            <IndexPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ ag-site/src/pages/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import Header from "../common/Header";
+import Footer from "../common/Footer";
+class IndexPage extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Header />
+        <h1>HI</h1>
+        <br />
+        <br />
+        <br />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
