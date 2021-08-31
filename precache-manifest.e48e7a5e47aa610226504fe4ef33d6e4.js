6a8f36560b2d1a8947404b22e14ff7cbd361d76d
Jon3123 <39013026+Jon3123@users.noreply.github.com>
2021-08-31 14:26:26 +0000

    Deploying to master from  @ d62e3eaf9d6855536a93616abb61a74568a64685 🚀

---
+++ precache-manifest.e48e7a5e47aa610226504fe4ef33d6e4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e97d0b26bc7a54cf32024ffb2ed7b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c195aaa27feaf0d5fe0",
+    "url": "/static/css/2.4364cc03.chunk.css"
+  },
+  {
+    "revision": "414712396759b56ebad3",
+    "url": "/static/css/main.d8999080.chunk.css"
+  },
+  {
+    "revision": "8c195aaa27feaf0d5fe0",
+    "url": "/static/js/2.f23744cf.chunk.js"
+  },
+  {
+    "revision": "5673193d9de73fdb0bf1adaec380368e",
+    "url": "/static/js/2.f23744cf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "414712396759b56ebad3",
+    "url": "/static/js/main.e20aead1.chunk.js"
+  },
+  {
+    "revision": "993695f9309fd49d512c",
+    "url": "/static/js/runtime-main.baf19579.js"
+  },
+  {
+    "revision": "98d2f97305ab25d4511982e73740922e",
+    "url": "/static/media/futura-light.98d2f973.ttf"
+  },
+  {
+    "revision": "de7855763715bde358c677122c27887b",
+    "url": "/static/media/hero-image.de785576.png"
+  },
+  {
+    "revision": "af3367765c4a888c8b40872e2ed71070",
+    "url": "/static/media/logo-text-white.af336776.png"
+  },
+  {
+    "revision": "deeb7ee7459d340bb0da124d1b6ebc99",
+    "url": "/static/media/logo-text.deeb7ee7.png"
+  },
+  {
+    "revision": "b98efbd97a8a479cf76a5b870b5b84cf",
+    "url": "/static/media/photo-reel.b98efbd9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3cd7b319efa3d67227934fbcf1e45b7.js"
+  "/precache-manifest.e48e7a5e47aa610226504fe4ef33d6e4.js"
 );
 
 self.addEventListener('message', (event) => {
